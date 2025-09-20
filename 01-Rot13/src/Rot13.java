d14ebd999e42c43225755ae23e5f853475604fe3
markTimblau <2024_mark.timblau@iticbcn.cat>
2025-09-20 18:56:16 +0200

    Ejercicio terminado, arrayList eliminada.

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -3,38 +3,38 @@ public class Rot13 {
     public static void main(String[] args) {
         //ESTOS SON LAS PRUEVAS
         String[] test = {"ABC","XYZ","Hola, Mr. calçot","Perdó, per tu què és?"};
+        
         System.out.println("Xifrat \n ------");
-        
         //CODIFICAR
-        String[] result = split(test);
-        
-        //RESULTADO
-        //PONER ESTO EN OTRA CLASE PARA NO TENER QUE ESCRIBIRLO OTRA VEZ
-        for (int i = 0; i < test.length; i++){
-            System.out.println(test[i] + " => " + result[i]);
-        }
+        String[] encriptado = split(test, true);
+        //MOSTRAR
+        show(test, encriptado);
+
+        System.out.println("Desxifrat \n ------");
         //DECODIFICAR
-        String[] result2 = split(result);
+        String[] desencriptado = split(encriptado, false);
+        //MOSTRAR
+        show(encriptado, desencriptado);
     }
     //DIVIDIMOS CADA TEST CADA TEST
-    public static String[] split(String[] test){
-        String resultTemp = "";
+    public static String[] split(String[] test, boolean type){
+
+        String resultat = "";
         for (int i = 0; i < test.length; i++){
             //METEMOS CADA CHAR DEL TEST DENTRO DE ENCRIPT
             char[] encript = test[i].toCharArray();
-            resultTemp += show(encript)  +",";
-            //System.out.println(test[i] + " => " + resultTemp);
+            //DIVIDIMOS LOS TEST POR '
+            resultat += "" + mayOrMin(encript, type) + "'";
         }
-        //NO USAR COMAS DARÁ ERROR--------------------------
-        String[] result = resultTemp.split(",");
-        //--------------------------------------------------
+        //CONVERTIMOS EL STRING EN UN ARRAY PARA DIVIDIR LOS TESTS
+        String[] result = resultat.split("'");
         return result;
     }
     //MUESTRA EL RESULTADO ENCRIPTADO
-    public static String show(char[] encript){
+    public static String mayOrMin(char[] encript, boolean type){
         //ARRAYS ENCRIPTADOS
-        char[] encriptionLower = "ïxàrwéühóaqjzçbèälñgdyskömáuïívpnúeçtòfc".toCharArray();
-        char[] encriptionUpper = "ïxàrwéühóaqjzçbèälñgdyskömáuïívpnúeçtòfc".toUpperCase().toCharArray();
+        char[] encriptionLower = "xàrwétühóaqjzçbèäloñgdysköimáuïívpnúeòfc".toCharArray();
+        char[] encriptionUpper = "xàrwétühóaqjzçbèäloñgdysköimáuïívpnúeòfc".toUpperCase().toCharArray();
         //DONDE GUARDAREMOS EL TEXTO ENCRIPTADO
         String result ="";
         //MIRAMOS CADA CARACTER
@@ -42,11 +42,11 @@ public class Rot13 {
             //ES UNA LETRA?
             if (Character.isLetter(encript[i])){
                 if (Character.isLowerCase(encript[i])){
-                    //SI ES MAYUSCULA
-                    result+= encript(encript[i],encriptionLower);
-                } else {
                     //SI ES MINUSCULA
-                    result+= encript(encript[i],encriptionUpper);
+                    result+= encript(encript[i],encriptionLower, type);
+                } else {
+                    //SI ES MAYUSCULA
+                    result+= encript(encript[i],encriptionUpper, type);
                 }
             } else {
                 //NO ES UNA LETRA
@@ -56,23 +56,37 @@ public class Rot13 {
         return result;
     }
     //ENCRIPTA EL TEXTO
-    public static char encript(char letter, char[] type){
+    public static char encript(char letter, char[] mayMin, boolean type){
         char result = ' ';
-        for (int i = 0; i < type.length; i++){
+        for (int i = 0; i < mayMin.length; i++){
             //  ENCONTRAMOS LA LETRA
-            if (letter == type[i]){
-                
-                if (i + 13 >= type.length){
-                    //SOBRESALE DEL CODIGO
-                    result = type[i+13-type.length];
+            if (letter == mayMin[i]){
+                //ENCRIPTAMOS O DESENCRIPTAMOS?
+                if (type){
+                    if (i + 13 >= mayMin.length){
+                        //SOBRESALE DEL CODIGO
+                        result = mayMin[i+13 - mayMin.length];
+                    } else {
+                        //no SOBRESALE
+                        result = mayMin[i+13];
+                    }
                 } else {
-                    //no SOBRESALE
-                    result = type[i+13];
+                    if (i - 13 < 0){
+                        //SOBRESALE DEL CODIGO
+                        result = mayMin[i-13 + mayMin.length];
+                    } else {
+                        //no SOBRESALE
+                        result = mayMin[i-13];
+                    }                    
                 }
             }
         }
         return result;
     }
+    //MUESTRA EL RESULTADO
+    public static void show(String[] start, String[] finish){
+       for (int i = 0; i < start.length; i++){
+           System.out.println(start[i] + " => " + finish[i]);
+       }
+    }
 }
-
-//REVISAR QUE HACIA MOD PARA LAS DIVISIONES
